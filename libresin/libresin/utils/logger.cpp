90e63cfc302f53fef469dd062ec807bb46fb906a
Michał Okurowski <micokoku@gmail.com>
2024-10-31 16:47:52 +0100

    Logger redesign

--- libresin/libresin/utils/logger.cpp
+++ libresin/libresin/utils/logger.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <libresin/utils/logger.hpp>
 #include <optional>
 #include <print>
@@ -7,81 +6,48 @@ namespace resin {
 
 std::optional<Logger> Logger::s_instance_ = std::nullopt;
 
-void Logger::init(LogLevel max_level,
-                  const std::optional<std::filesystem::path>& log_file,
-                  LogLevel show_func_max_level, bool print_on_stdout) {
+static void vprint(FILE* stream, std::string_view fmt, std::format_args args) {
+  if constexpr (std::__unicode::__literal_encoding_is_utf8()) {
+    std::vprint_unicode(stream, fmt, args);
+  } else {
+    std::vprint_nonunicode(stream, fmt, args);
+  }
+}
+
+static void vprint(std::string_view fmt, std::format_args args) { vprint(stdout, fmt, args); }
+
+LoggerScribe::LoggerScribe(LogLevel max_level) : max_level_(max_level) {}
+
+TerminalLoggerScribe::TerminalLoggerScribe(LogLevel max_level, bool use_stderr)
+    : LoggerScribe(max_level), use_stderr_(use_stderr) {}
+
+void TerminalLoggerScribe::vlog(std::string_view usr_fmt, std::format_args usr_args, const std::tm& date_time,
+                                const std::source_location& location, std::string_view location_file_name,
+                                LogLevel level) {
+  if (level > max_level_) {
+    return;
+  }
+
+  std::print("[{0} {1}:{2}:{3}] ", get_log_prefix(level), date_time.tm_hour, date_time.tm_min, date_time.tm_sec);
+
+  if (level < LogLevel::Info) {
+    std::print("{0}({1}:{2}) `{3}`\n", location.file_name(), location.line(), location.column(),
+               location.function_name());
+  }
+
+  vprint(usr_fmt, usr_args);
+  std::print("\n");
+}
+
+void Logger::init() {
   if (s_instance_.has_value()) {
-    Logger::err("Logger must not be initialized more than once.");
+    std::print(stderr, "Logger must not be initialized more than once!");
     std::abort();
   }
 
-  s_instance_.emplace(max_level, log_file, show_func_max_level,
-                      print_on_stdout);
-}
-
-Logger::Logger(LogLevel max_level,
-               const std::optional<std::filesystem::path>& log_file,
-               LogLevel show_func_max_level, bool print_on_stdout)
-    : max_level_(max_level),
-      log_file_(std::nullopt),
-      show_func_max_level_(show_func_max_level),
-      print_on_stdout_(print_on_stdout) {
-  if (log_file.has_value()) {
-    log_file_.emplace(log_file.value(), std::ios::ate);
-
-    if (!log_file_.value()) {
-      std::println(std::cerr, "Could not open the log file {0}",
-                   log_file.value().c_str());
-      log_file_ = std::nullopt;
-    }
-  }
+  s_instance_.emplace();
 }
 
 Logger::~Logger() {}
-
-void Logger::emit_msg(LogLevel level, std::string_view msg,
-                      const std::source_location& location) {
-  if (level > max_level_ || max_level_ == LogLevel::None ||
-      level == LogLevel::None) {
-    return;
-  }
-
-  if (!print_on_stdout_ && !log_file_.has_value()) {
-    return;
-  }
-
-  std::lock_guard<std::mutex> lock(mutex_);
-
-  static std::array<const char*, 3> s_prefixes = {
-      "ERROR",
-      "WARNING",
-      "INFO",
-  };
-
-  std ::time_t epoch = std::time(nullptr);
-  std::tm* lt        = std::localtime(&epoch);
-
-  auto log_message = [&](std::ostream& stream) {
-    std::print(stream, "[{0} {1}:{2}:{3}]",
-               s_prefixes[static_cast<uint32_t>(level) - 1], lt->tm_hour,
-               lt->tm_min, lt->tm_sec);
-
-    if (level <= show_func_max_level_) {
-      std::print(stream, " {0}({1}:{2}) `{3}`", location.file_name(),
-                 location.line(), location.column(), location.function_name(),
-                 msg);
-    }
-
-    std::print(stream, ": {0}\n", msg);
-  };
-
-  if (print_on_stdout_) {
-    log_message(std::cout);
-  }
-
-  if (log_file_.has_value()) {
-    log_message(log_file_.value());
-  }
-}
 
 }  // namespace resin
