be8420aca6eb443068eed5b22cfaf8626b36b94a
KuZu <jakubzuchowski13@gmail.com>
2024-11-10 23:22:53 +0100

    Event class

---
+++ resin/resin/event/event.hpp
@@ -0,0 +1,109 @@
+#ifndef EVENT_HPP
+#define EVENT_HPP
+#include <vcruntime_typeinfo.h>
+
+#include <concepts>
+#include <functional>
+#include <string>
+#include <unordered_map>
+
+#define EVENT_NAME(eventType)                                \
+  static constexpr const char* name() { return #eventType; } \
+  static constexpr std::string to_string() { return name(); }
+
+#define BIND_EVENT_METHOD(fn) \
+  [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
+
+namespace resin {
+
+enum class EventType {
+  None = 0,
+};
+
+class BaseEvent {
+ public:
+  BaseEvent() = default;
+  virtual ~BaseEvent() = default;
+
+  virtual EventType event_type() const = 0;
+
+  BaseEvent(const BaseEvent&) = delete;
+  BaseEvent(BaseEvent&&) = delete;
+  BaseEvent& operator=(const BaseEvent&) = delete;
+  BaseEvent& operator=(BaseEvent&&) = delete;
+
+  bool handled = false;
+};
+
+template <EventType Type>
+class Event : public BaseEvent {
+ public:
+  static constexpr EventType type() { return Type; }
+  constexpr EventType event_type() const override { return type(); }
+};
+
+template <typename E>
+concept EventConcept = std::derived_from<E, BaseEvent> && requires(E a) {
+  { E::name() } -> std::same_as<const char*>;
+  { E::to_string() } -> std::same_as<std::string>;
+  { E::type() } -> std::same_as<EventType>;
+};
+
+template <typename T, typename E>
+concept EventHandler = EventConcept<E> && requires(T t, E& e) {
+  { t(e) } -> std::same_as<bool>;
+};
+
+class EventDispatcher {  // TODO(kuzu): implement event bus and event section of app loop
+ public:
+  template <typename E>
+  using callback_t = std::function<bool(E&)>;
+
+  template <EventConcept E, EventHandler<E> F>
+  void subscribe(const F& callback_fn) {
+    const EventType type = E::type();
+    subscribers_[type].push_back(CallbackWrapper<E>(callback_fn));
+  }
+
+  template <EventConcept E>
+  bool dispatch(E& event) {
+    const EventType type = E::type();
+    for (auto& callback : subscribers_[type]) {
+      event.handled |= callback(event);
+      if (event.handled) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool dispatch(BaseEvent& event) {
+    const EventType type = event.event_type();
+    for (auto& callback : subscribers_[type]) {
+      event.handled |= callback(event);
+      if (event.handled) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+ private:
+  std::unordered_map<EventType, std::vector<callback_t<BaseEvent>>> subscribers_;
+
+  template <EventConcept E>
+  class CallbackWrapper {
+   public:
+    template <EventHandler<E> F>
+    explicit CallbackWrapper(const F& callback) : callback_(callback) {}
+
+    bool operator()(BaseEvent& event) { return callback_(static_cast<E&>(event)); }
+
+   private:
+    callback_t<E> callback_;
+  };
+};
+
+}  // namespace resin
+
+#endif  // EVENT_HPP
