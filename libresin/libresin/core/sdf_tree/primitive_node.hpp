dc89451fa8e71f8faa202df213c03d78d455e8c9
Michał Okurowski <micokoku@gmail.com>
2024-12-08 13:08:16 +0100

    Primitive nodes refactor

--- libresin/libresin/core/sdf_tree/primitive_node.hpp
+++ libresin/libresin/core/sdf_tree/primitive_node.hpp
@@ -10,15 +10,40 @@
 
 namespace resin {
 
+using SDFTreeShaderComponent = sdf_shader_consts::SDFShaderComponents;
+using SDFTreePrimitive       = sdf_shader_consts::SDFShaderPrim;
+constexpr EnumMapping<SDFTreePrimitive> kSDFTreePrimitiveNames({"Sphere", "Cube"});
+
 class PrimitiveNode : public SDFTreeNode {
  public:
+  constexpr static auto primitive_names() { return kSDFTreePrimitiveNames.names_; }
+
   inline void accept_visitor(ISDFTreeNodeVisitor& visitor) override { visitor.visit_primitive(*this); }
   bool is_leaf() final { return true; }
+
+  virtual SDFTreePrimitive primitive_type() const       = 0;
+  virtual SDFTreeShaderComponent component_type() const = 0;
+
+  std::string_view primitive_name() const { return kSDFTreePrimitiveNames.get_name(primitive_type()); }
 
   ~PrimitiveNode() override = default;
   explicit PrimitiveNode(SDFTreeRegistry& tree, std::string_view name) : SDFTreeNode(tree, name) {}
 
+  inline std::string gen_shader_code() const final {
+    return std::format(
+        "{}({}[{}]*{},{}[{}])", sdf_shader_consts::kSDFShaderPrimFunctionNames.get_name(primitive_type()),
+        sdf_shader_consts::kSDFShaderComponentArrayNames.get_name(
+            sdf_shader_consts::SDFShaderComponents::Transforms),                                              //
+        transform_id_.raw(),                                                                                  //
+        sdf_shader_consts::kSDFShaderVariableNames.get_name(sdf_shader_consts::SDFShaderVariable::Position),  //
+        sdf_shader_consts::kSDFShaderComponentArrayNames.get_name(component_type()),                          //
+        get_component_raw_id()                                                                                //
+    );
+  }
+
  protected:
+  virtual size_t get_component_raw_id() const = 0;
+
   inline void insert_leaves_to(
       std::unordered_set<IdView<SDFTreeNodeId>, IdViewHash<SDFTreeNodeId>, std::equal_to<>>& leaves) final {
     leaves.emplace(node_id());
@@ -30,20 +55,6 @@ class PrimitiveNode : public SDFTreeNode {
   }
 
   void push_dirty_primitives() final;
-
-  inline std::string get_function_call_code(sdf_shader_consts::SDFShaderPrim primitive,
-                                            sdf_shader_consts::SDFShaderComponents component,
-                                            size_t component_id) const {
-    return std::format(
-        "{}({}[{}]*{},{}[{}])", sdf_shader_consts::kSDFShaderPrimFunctionNames.get_name(primitive),
-        sdf_shader_consts::kSDFShaderComponentArrayNames.get_name(
-            sdf_shader_consts::SDFShaderComponents::Transforms),                                              //
-        transform_id_.raw(),                                                                                  //
-        sdf_shader_consts::kSDFShaderVariableNames.get_name(sdf_shader_consts::SDFShaderVariable::Position),  //
-        sdf_shader_consts::kSDFShaderComponentArrayNames.get_name(component),                                 //
-        component_id                                                                                          //
-    );
-  }
 };
 
 class SphereNode;
@@ -56,25 +67,26 @@ class SphereNode final : public PrimitiveNode {
     visitor.visit_sphere(*this);
   }
 
-  inline std::string gen_shader_code() const override {
-    return get_function_call_code(sdf_shader_consts::SDFShaderPrim::Sphere,
-                                  sdf_shader_consts::SDFShaderComponents::Spheres, sphere_id_.raw());
-  }
+  explicit SphereNode(SDFTreeRegistry& tree, float _radius = 1.F);
+
+  inline SDFTreePrimitive primitive_type() const override { return SDFTreePrimitive::Sphere; }
+  inline SDFTreeShaderComponent component_type() const override { return SDFTreeShaderComponent::Spheres; }
 
   ~SphereNode() override = default;
   inline IdView<SphereNodeId> component_id() const { return sphere_id_; }
-  explicit SphereNode(SDFTreeRegistry& tree, float _radius = 1.F);
 
   [[nodiscard]] inline std::unique_ptr<SDFTreeNode> copy() override {
     return std::make_unique<SphereNode>(tree_registry_, radius);
   }
+
+ protected:
+  inline size_t get_component_raw_id() const override { return sphere_id_.raw(); }
 
  public:
   float radius;
 
  private:
   SphereNodeId sphere_id_;
-  std::string name_;
 };
 
 class CubeNode;
@@ -87,23 +99,26 @@ class CubeNode final : public PrimitiveNode {
     visitor.visit_cube(*this);
   }
 
-  [[nodiscard]] inline std::string gen_shader_code() const override {
-    return get_function_call_code(sdf_shader_consts::SDFShaderPrim::Cube, sdf_shader_consts::SDFShaderComponents::Cubes,
-                                  cube_id_.raw());
-  }
+  explicit CubeNode(SDFTreeRegistry& tree, float _size = 1.F);
+
+  inline SDFTreePrimitive primitive_type() const override { return SDFTreePrimitive::Cube; }
+  inline SDFTreeShaderComponent component_type() const override { return SDFTreeShaderComponent::Cubes; }
 
   ~CubeNode() override = default;
-  explicit CubeNode(SDFTreeRegistry& tree, float _size = 1.F);
   inline IdView<CubeNodeId> component_id() const { return cube_id_; }
 
-  inline std::unique_ptr<SDFTreeNode> copy() override { return std::make_unique<CubeNode>(tree_registry_, size); }
+  [[nodiscard]] inline std::unique_ptr<SDFTreeNode> copy() override {
+    return std::make_unique<CubeNode>(tree_registry_, size);
+  }
+
+ protected:
+  inline size_t get_component_raw_id() const override { return cube_id_.raw(); }
 
  public:
   float size;
 
  private:
   CubeNodeId cube_id_;
-  std::string name_;
 };
 
 }  // namespace resin
